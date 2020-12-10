85738bb9411bbadf608d9610a6bb893484d990e1
Husnain Javed <me@husnainjaved.com>
2020-12-10 12:16:13 +0000

    Create staging environment

--- src/components/OrderPlaced.vue
+++ src/components/OrderPlaced.vue
@@ -1,19 +1,15 @@
 <template>
   <div id="order-placed" class="mx-auto">
     <div id="order-placed__main" class="p-8 sm:p-16 text-left grid auto-rows-auto gap-8 items-center">
-      <div class="p-8 bg-green bg-opacity-10 rounded">
+      <div class="p-4 bg-green bg-opacity-10 rounded">
         <h2>You order has been placed!</h2>
         <p class="mt-2">An order confirmation will be sent to your email address shortly.</p>
       </div>
-      <div v-if="order">
+      <div v-if="shipping">
         <h3>Shipping address</h3>
-        <p class="my-2"><strong>{{ order.shipping.name }}</strong></p>
-        <ul class="text-sm">
-          <li>{{ order.shipping.address.line1 }}, {{ order.shipping.address.line2 }}</li>
-          <li>{{ order.shipping.address.city }}</li>
-          <li>{{ order.shipping.address.postal_code }}</li>
-          <li>{{ order.shipping.address.state }}</li>
-          <li>{{ order.shipping.address.country }}</li>
+        <ul class="my-2 text-sm">
+          <li class="font-bold">{{ shipping.name }}</li>
+          <li v-for="(line, index) in shipping.address" :key="index">{{ line }}</li>
         </ul>
       </div>
       <div>
@@ -31,7 +27,7 @@ export default {
   name: 'OrderPlaced',
   data() {
     return {
-      order: null
+      shipping: null
     }
   },
   mounted() {
@@ -42,7 +38,7 @@ export default {
     async getOrderDetails(id) {
       try {
         const response = await axios.get(`${process.env.VUE_APP_API_URL}/get-order`, { params: { id } });
-        this.order = response.data;
+        this.shipping = response.data;
       } catch (err) {
         return;
       }
@@ -50,7 +46,3 @@ export default {
   }
 }
 </script>
-
-<style>
-
-</style>
